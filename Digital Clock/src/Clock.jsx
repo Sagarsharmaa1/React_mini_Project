a91142f4623abf4a5654182ee88ace65ea96d4ad
Sagarsharmaa1 <153342678+Sagarsharmaa1@users.noreply.github.com>
2025-12-22 16:53:24 +0000

    fix digital clock time formatting

--- Digital Clock/src/Clock.jsx
+++ Digital Clock/src/Clock.jsx
@@ -4,11 +4,6 @@ import { useState,useEffect } from "react";
 function Clock(){
    const[time,settime]= useState(new Date());
 
-   const hour = time.getHours()
-   const min = time.getMinutes()
-   const sec = time.getSeconds()
-
-
     useEffect(()=>{
         const intervalId = setInterval(()=>{
             settime(new Date());
@@ -22,14 +17,17 @@ function Clock(){
     },[]);
 
     function formattime(){
+
+    const hours = time.getHours().toString().padStart(2, "0");
+    const minutes = time.getMinutes().toString().padStart(2, "0");
+    const seconds = time.getSeconds().toString().padStart(2, "0");
+
         return `${hour}: ${min}: ${sec}`
     }
     return(
         <div>
             <p>Digital Clock</p>
-           
-           {formattime()}
-
+            <h2>{formatTime()}</h2>
         </div>
     )
 }
